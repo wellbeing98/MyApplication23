d4beeac9a61fbde4cba10251025d624cfcd3f469
wellbeing98 <you@example.com>
2021-11-12 01:12:35 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplication23/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.myapplication23;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth nFirebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nFirebaseAuth = FirebaseAuth.getInstance();
+
+        Button btn_logout = findViewById(R.id.btn_logout);
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //로그아웃 하기
+                nFirebaseAuth.signOut();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+    }
+}
